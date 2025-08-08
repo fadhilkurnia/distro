b0149a8ab532c825e07fdc6202693e53697a170e
ThePlatypus-Person <Ornithorhynchus0920@protonmail.com>
2025-08-09 03:30:38 +0700

    Add language to data store, add documentation

--- sut/ailidani.paxi/run.py
+++ sut/ailidani.paxi/run.py
@@ -32,7 +32,13 @@ PROTOCOLS = [{"num": 1, "text": "paxos"},
 jobs = []
 
 
-def run_command(cmd):
+def run_command(cmd) -> None:
+    """
+    Runs a command in a new subprocess which will be tracked inside jobs list.
+
+    :param cmd: Command line arguments
+    :type cmd: str[]
+    """
     proc = subprocess.Popen(cmd)
     jobs.append({
         'cmd': cmd,
@@ -42,7 +48,16 @@ def run_command(cmd):
     proc.wait()
 
 
-def main(run_ycsb):
+def main(run_ycsb) -> None:
+    """
+    Main function called by the root main.py script.
+    Gives user a choice to start/stop paxi instances
+    and to run the YCSB benchmark on the instance.
+
+    :param run_ycsb: Function to run YCSB benchmark. Takes in protocol
+                     data {name, language} and YCSB interface name as argument.
+    :type run_ycsb: Callable[dict[str, str], str]
+    """
     selected_protocol = None
     while True:
         val = helper.get_option(0, len(OPTIONS) - 1, OPTIONS)
@@ -51,7 +66,10 @@ def main(run_ycsb):
         match val:
             case 0:
                 prot_num = helper.get_option(1, len(PROTOCOLS), PROTOCOLS)
-                selected_protocol = PROTOCOLS[prot_num-1]["text"]
+                selected_protocol = {
+                    "name": PROTOCOLS[prot_num-1]["text"],
+                    "language": "Go",
+                }
                 start_paxi(PAXI_BIN, selected_protocol)
             case 1:
                 stop_paxi()
@@ -59,17 +77,25 @@ def main(run_ycsb):
                 run_ycsb(selected_protocol, "paxi")
 
 
-def start_paxi(path, protocol):
-    # List of commands to run
+def start_paxi(path, protocol) -> None:
+    """
+    Runs the paxi instances with the specified protocol in different
+    threads concurrently. Currently only supports local startup.
+
+    :param path: Path to bin/ directory inside the paxi repository.
+    :type path: Path
+    :param protocol: Protocol data that is being started.
+    :type protocol: dict[str, str]
+    """
     server = path / "server"
     config = CURR_DIR / "config.json"
 
     commands = [
-        [server, "-id", "1.1", f"-algorithm={protocol}", "-config", config],
-        [server, "-id", "1.2", f"-algorithm={protocol}", "-config", config],
-        [server, "-id", "1.3", f"-algorithm={protocol}", "-config", config],
-        [server, "-id", "2.1", f"-algorithm={protocol}", "-config", config],
-        [server, "-id", "2.2", f"-algorithm={protocol}", "-config", config],
+        [server, "-id", "1.1", f"-algorithm={protocol['name']}", "-config", config],
+        [server, "-id", "1.2", f"-algorithm={protocol['name']}", "-config", config],
+        [server, "-id", "1.3", f"-algorithm={protocol['name']}", "-config", config],
+        [server, "-id", "2.1", f"-algorithm={protocol['name']}", "-config", config],
+        [server, "-id", "2.2", f"-algorithm={protocol['name']}", "-config", config],
     ]
 
     # Start each command in its own thread
@@ -77,11 +103,14 @@ def start_paxi(path, protocol):
         print(f"Starting: {' '.join(map(str, cmd))}")
         t = threading.Thread(target=run_command, args=(cmd,))
         t.start()
-    print("Paxi instances successfully started")
+    print(f"Paxi {protocol['name']} instances successfully started")
 
 
-def stop_paxi():
-    # Terminate all still-running processes
+def stop_paxi() -> None:
+    """
+    Terminates all running instances of paxi that are still recorded inside
+    the jobs list, then removes all the logfiles created by the instances.
+    """
     for job in jobs:
         proc = job['process']
         if proc.poll() is None:  # Still running
